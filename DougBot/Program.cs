96b4b4280ad2208b3ac8b9bdd105d9245c731baa
Malikil <eboy.coltt5@gmail.com>
2018-04-02 01:35:17 -0700

    Ping Pong
    Added basic framework, bot will now connect and can respond to !ping

--- DougBot/Program.cs
+++ DougBot/Program.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Net.Providers.WS4Net;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DougBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().Start().GetAwaiter().GetResult();
+
+        private ISocketMessageChannel outchannel;
+
+        public async Task Start()
         {
-            Console.WriteLine("Hello World!");
+            // Create the client object, set up logging and receiving messages
+            DiscordSocketClient client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                WebSocketProvider = WS4NetProvider.Instance
+            });
+            client.Log += Log;
+            client.MessageReceived += Message;
+
+            // Login and start client using token from file
+            string token = GetToken();
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Wait until program end
+            while (true)
+            {
+                Console.Write(" > ");
+                string cmd = Console.ReadLine();
+                if (cmd.ToLower().Equals("quit") || cmd.ToLower().Equals("exit"))
+                    break;
+                else if (outchannel != null)
+                    await outchannel.SendMessageAsync(cmd);
+            }
+            await client.StopAsync();
+            await client.LogoutAsync();
+        }
+
+        private async Task Message(SocketMessage message)
+        {
+            if (message.Content.Equals("!ping"))
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+            else if (message.Content.Equals("!talkhere"))
+            {
+                outchannel = message.Channel;
+                await outchannel.SendMessageAsync("ok");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private String GetToken()
+        {
+            String token;
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader("..\\..\\..\\..\\..\\token.txt");
+                token = reader.ReadLine();
+            }
+            catch (IOException)
+            {
+                token = "";
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+            return token;
         }
     }
 }
